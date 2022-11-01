e954b606d4947d966bbf67ce05903e0818ccbf19
HoKhaiQuang <hokhaiquang231@gmai.com>
2022-11-02 00:39:16 +0700

    update midđlewẳ

--- middleware/authTokenMiddleware.js
+++ middleware/authTokenMiddleware.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 module.exports = async (req, res, next) => {
 
     const token =
-        req.body.token || req.query.token || req.headers["x-access-token"];
+        req.body.token || req.query.token || req.headers["x-access-token"] || req.header('Authorization').replace('Bearer ', '');
 
     if (!token) {
         return res.status(403).json({
